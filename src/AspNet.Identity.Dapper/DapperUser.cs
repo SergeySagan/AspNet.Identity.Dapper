ccc384f806b520d184e954ed4b42a3ec145111c1
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-12-14 14:50:15 +0200

    initial load

---
+++ src/AspNet.Identity.Dapper/DapperUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace AspNet.Identity.Dapper
+{
+    public class DapperUser : IUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ src/AspNet.Identity.Dapper/DapperUserStore.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNet.Identity.Dapper
+{
+    public class DapperUserStore : IUserStore<DapperUser>
+    {
+        // IUserStore
+
+        public Task CreateAsync(DapperUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(DapperUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DapperUser> FindByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DapperUser> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(DapperUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        // IDisposable
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
